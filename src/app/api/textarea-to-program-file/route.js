cfb95fae6c5fbb565fe3e9e060f3abf0695c0a87
jordannakamoto <jordannakamoto@gmail.com>
2024-04-07 22:03:26 -0700

    promises

--- src/app/api/textarea-to-program-file/route.js
+++ src/app/api/textarea-to-program-file/route.js
@@ -6,33 +6,31 @@
 // for program run
 
 import { NextResponse } from "next/server";
-import fs from 'fs';
+import fs from 'fs/promises'; // Import the promise-based version of the fs module
 import path from 'path';
 
-export const dynamic = 'force-dynamic'
+export const dynamic = 'force-dynamic';
 
 export async function POST(request) {
-    const data = await request.json()
-    const filePath = path.join(process.cwd(),'Program/tests/inputs/interface.c');
-    console.log("writing file from frontend to:", filePath);
+    const data = await request.json();
+    const filePath = path.join(process.cwd(), 'Program/tests/inputs/interface.c');
+    console.log("Writing file from frontend to:", filePath);
 
     const text = data.text;
 
-    // console.log(text);
-
     try {
-      fs.writeFileSync(filePath, text);
-      // File write was successful
-
-      return NextResponse.json({
-          message: "Frontend to program file interface.c written successfully",
-          filePath: filePath,
-      }, { status: 200 });
-  } catch (err) {
-      console.error('Error writing to file:', err);
-      // File write failed
-      return NextResponse.json({
-          error: "Failed to write file from frontend"
-      }, { status: 500 });
-  }
+        // Use fs.promises to write file asynchronously
+        await fs.writeFile(filePath, text);
+        // File write was successful
+        return NextResponse.json({
+            message: "Frontend to program file interface.c written successfully",
+            filePath: filePath,
+        }, { status: 200 });
+    } catch (err) {
+        console.error('Error writing to file:', err);
+        // File write failed
+        return NextResponse.json({
+            error: "Failed to write file from frontend"
+        }, { status: 500 });
+    }
 }
